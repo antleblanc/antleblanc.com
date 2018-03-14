b5901f16eab3a0f44c5a0da5bcbb1c7760ce5173
Antoine Leblanc <ant.leblanc@gmail.com>
2018-03-14 14:58:38 +0100

    remove headroom deps.

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -4,17 +4,6 @@
   class MyApp {
     constructor() {
       this.document = document;
-      this.header = this.document.querySelector('#header');
-      this.navigation = this.document.querySelector('#navigation');
-    }
-
-    handleHeader() {
-      const headroom = new Headroom(this.navigation, {
-        offset: this.header.offsetHeight - 120,
-        tolerance: 5
-      });
-
-      return headroom.init();
     }
 
     initServiceWorker() {
@@ -50,7 +39,6 @@
     run() {
       document.querySelector('html').classList.remove('no-js');
       document.querySelector('html').classList.add('js');
-      this.handleHeader();
       this.initServiceWorker();
     }
   }
